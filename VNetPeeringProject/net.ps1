e9f93c77e25fc888197edf0fed4ddf2d88698a5c
Olmanarias <onacariascuevas@gmail.com>
2023-06-29 09:19:21 -0600

    creando un peering secundario en la region weast y se conecto con el perring primario y se comprobo la comexion en las vm

--- VNetPeeringProject/net.ps1
+++ VNetPeeringProject/net.ps1
@@ -12,7 +12,7 @@ $subnetname2 = "subnet2"
 $prefix2 = "10.1.0.0/16"
 $subnetprefix2 = "10.1.0.0/24"
 $peering = "emparejamiento"
-$peering2 = "emparejamiento"
+$peering2 = "emparejamiento2"
 $vm1 = "virtualmachineeast"
 $vm02= "virtualmachinewestus"
 $adminname1 = "olmannoe"
@@ -40,9 +40,11 @@ $vnet2Id=$(az network vnet show --resource-group $resourcegroup2 --name $namevne
 #crear un peering
 az network vnet peering create --resource-group $resourcegroup --name $peering --vnet-name $namevnet --remote-vnet $vnet2Id --allow-forwarded-traffic yes --allow-vnet-access yes
 
+#crear un peering
+az network vnet peering create --resource-group $resourcegroup2 --name $peering2 --vnet-name $namevnet2 --remote-vnet $vnet1Id --allow-forwarded-traffic yes --allow-vnet-access yes
+
 #Crear una vm unido en la primera net
 az vm create --resource-group $resourcegroup --name $VM1 --location $location --image $image --vnet-name $namevnet --size Standard_B1s --admin-username $adminname1 --admin-password $adminpass1 --subnet $subnetname --subnet-address-prefix $subnetprefix
-
 
 #Crear una vm unido en la segunda net
 az vm create --resource-group $resourcegroup2 --name $VM02 --location $location2 --image $image --vnet-name $namevnet2 --size Standard_B1s --admin-username $adminname2 --admin-password $adminpass2 --subnet $subnetname2 --subnet-address-prefix $subnetprefix2
